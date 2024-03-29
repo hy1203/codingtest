fdb6056736831a09484b73c8b4c87c0a4c851aef
hy1203 <102866532+hy1203@users.noreply.github.com>
2024-03-29 15:16:51 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 508 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,24 @@
+while True:
+    sen = input()
+    stack = []
+    if sen == '.':
+        break
+    for i in sen:
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(']')
+                break
+        elif i == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
+                break
+    if len(stack) == 0:
+        print('yes')
+    else:
+        print('no')
